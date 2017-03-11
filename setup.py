0f8f328797aceb684a4b4e2ae316038fd4946065
Katie <kjones@epfl.ch>
2017-03-11 16:22:15 +0100

    Add python install file

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Setup script for ink package
+from distutils.core import setup
+
+setup(name = 'Ink',
+      version = '1.0',
+      description = 'Backup manager',
+      author = 'Katie Jones',
+      author_email = 'k.jo133@gmail.com',
+      url = 'https://github.com/katie-jones/ink',
+      py_modules = ['ink']
+      )
