d74905b40280d0bddb0cdf0ca25c7fcfc74da075
Katie <kjones@epfl.ch>
2017-03-11 23:18:07 +0100

    Fix naming in setup.py

--- source/setup.py
+++ source/setup.py
@@ -1,7 +1,7 @@
 # Setup script for ink package
 from setuptools import setup
 
-setup(name = 'Ink',
+setup(name = 'ink',
       version = '1.0',
       description = 'Backup manager',
       author = 'Katie Jones',
@@ -9,6 +9,6 @@ setup(name = 'Ink',
       url = 'https://github.com/katie-jones/ink',
       py_modules = ['ink'],
       entry_points = {
-          'console_scripts': ['run-ink=ink:main_from_command_line']
+          'console_scripts': ['run_ink=ink:main_from_command_line']
       }
       )
